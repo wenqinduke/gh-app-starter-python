43cb7a1270fb273af3341e8726545f6af662d957
Prince S. Valluri <psvalluri@linkedin.com>
2019-11-26 16:58:52 -0800

    Working webhooks app.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# python-bot
+# Python Webhooks Bot
+
+This app is meant to serve as an application to help you onboard to the GitHub ecosystem and start using Webhooks.
+
+## How to set it up and use it
+* Generate a Personal Access Token on GitHub - https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
+* Clone/Fork this repo
+* Enter your GitHub Username and Access Token on Line 17 and 18 of app.py
+* Generate your virutal environment - `python3 -m venv .env`
+* Activate your environment - `source .env/bin/activate`
+* Install dependencies - `pip3 install -r requirements.txt`
+* Run the app - `python3 app.py`
+
+## How to use Webhooks
+* Create a new repository at https://github.com/li-playground/
+* Visit https://smee.io/ and click on `Start a new Channel` and note the URL
+* Go to your Repository's Settings -> Webhooks -> Add Webhook
+* Enter the Smee URL from the above step here and set Content Type to `application/json`, skip the secret and select `Send me everything`
+* Open a new terminal window and navigate to where you checked out this repo and activate virual environment as above - `source .env/bin/activate`
+* Run `pysmee forward <SMEE_URL> http://127.0.0.1:5000/webhook` - this will forward all events to your app.
+
+## See it in action
+* Go ahead and create a new Pull Request in the repository you just added the webhook to
+* Voila! A new comment should be added to it.
--- app.py
+++ app.py
@@ -1,52 +1,71 @@
 import json
 import logging
+import requests
+import sys
 
 from flask import Flask, request
 from objectify_json import ObjectifyJSON
+from requests.auth import HTTPBasicAuth
+
+from github_api import API_BASE_URL, make_github_api_call
 
 
 log = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(levelname)s:%(process)s:%(name)s:%(message)s')
 
-app = Flask(__name__)
+# Requied constants. Fill in your details. Do not check this in with these credentials.
+GITHUB_USER = ""
 GITHUB_TOKEN = ""
-API_BASE_URL = ""
+API_BASE_URL = "https://api.github.com"
 
+# Create the Flask App.
+app = Flask(__name__)
 
 
 @app.route('/webhook', methods=['POST'])
-def process_message(message):
-    log.info('Processing message...')
+def process_message():
+    log.info('Incoming webhook')
     webhook = ObjectifyJSON(request.json)
 
+    # Let's react only when a new Pull Requests has been opened.
     if request.headers['X-Github-Event'] == 'pull_request' and str(webhook.action).lower() == 'opened':
+        # This webhooks has this schema - https://developer.github.com/v3/activity/events/types/#pullrequestevent
         add_pr_comment(webhook)
+    else:
+        log.info("Irrelavant webhook.")
 
     return 'GOOD'
 
 
 def add_pr_comment(webhook):
-    log.info('Processing Trunk Status Check')
+    log.info('New Pull Request opened. Adding comment.')
+
+    # Gather the requried information from the payload to send a successful request to GitHub REST API.
     repo_full_name = str(webhook.repository.full_name)
-    pr_number = None
-    author = None
+    pr_number = str(webhook.pull_request.number)
 
     comments_url = f'{API_BASE_URL}/repos/{repo_full_name}/issues/{pr_number}/comments'
+    body = "Hello there, thanks for creating a new Pull Request!"
+
+    # Make the API call.
+    make_github_api_call(comments_url, 'POST', {'body': body})
 
 
 def make_github_api_call(url, method, params=None):
     """Send API call to Github using a personal token."""
-    headers = {'Accept': 'application/vnd.github.antiope-preview+json', 'Content-Type': 'application/json', 'Authorization': f'token {GITHUB_TOKEN}'}
 
-    if method.upper() == 'POST':
-        response = requests.post(url, headers=headers, data=json.dumps(params))
-    elif method.upper() == 'GET':
-        response = requests.get(url, headers=headers)
-    else:
-        raise Exception('Invalid Request Method.')
+    # Required headers.
+    headers = {'Accept': 'application/vnd.github.antiope-preview+json', 'Content-Type': 'application/json'}
 
-    print(response.status_code)
-    return json.loads(response.text)
+    try:
+        if method.upper() == 'POST':
+            response = requests.post(url, headers=headers, data=json.dumps(params), auth=HTTPBasicAuth(GITHUB_USER, GITHUB_TOKEN))
+        elif method.upper() == 'GET':
+            response = requests.get(url, headers=headers, auth=HTTPBasicAuth(GITHUB_USER, GITHUB_TOKEN))
+        else:
+            raise Exception('Invalid Request Method.')
+    except:
+        log.exception("Could not make a successful API call to GitHub.")
 
 
 if __name__ == '__main__':
